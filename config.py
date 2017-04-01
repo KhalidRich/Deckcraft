701b2435a3cc8398c34e71b97f4e2220d2e1ed10
Khalid Richards <khalidrichards@Khalids-MacBook-Pro.local>
2017-04-01 04:24:02 -0400

    Setting up directories and files.

---
+++ config.py
@@ -0,0 +1,8 @@
+import os
+
+# Database Configuration
+basedir = os.path.abspath(os.path.dirname(__file__))
+if os.environ.get('DATABASE_URL') is None:
+    DATABASE_URI = 'sqlite:///' + os.path.join(basedir, 'app.db')
+else:
+    DATABASE_URI = os.environ.get(DATABASE_URL)
